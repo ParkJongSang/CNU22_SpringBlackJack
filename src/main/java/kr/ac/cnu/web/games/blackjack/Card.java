0224f6994d1b9947c394e16d0720098e8275657b
Kim man seong <okms1017@gmail.com>
2018-06-08 05:47:32 +0900

    주석처리 및 getRank()추가

--- src/main/java/kr/ac/cnu/web/games/blackjack/Card.java
+++ src/main/java/kr/ac/cnu/web/games/blackjack/Card.java
@@ -5,17 +5,25 @@ import lombok.Data;
 
 /**
  * Created by rokim on 2018. 5. 26..
+ * Modified by manseongkim on 2018. 6. 8..
  */
+//카드구현
 @Data
 public class Card {
-    private final int rank;
-    private final Suit suit;
-
+    private final int rank; //숫자
+    private final Suit suit; //문양
+    //Constructor of Card
     public Card(int rank, Suit suit) {
+        //숫자가 13보다 크면 예외처리
         if (rank > 13) {
             throw new NoSuchRankException();
         }
+        //카드의 숫자와 문양 설정
         this.rank = rank;
         this.suit = suit;
+    }
+    //카드의 숫자를 읽어온다
+    public int getRank() {
+        return this.rank;
     }
 }
